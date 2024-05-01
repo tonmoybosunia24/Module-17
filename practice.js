a4f23673ceed85db285661ecee44aeb485c094d7
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2024-05-01 13:07:24 +0600

    Full Module Complete With Practice

--- practice.js
+++ practice.js
@@ -30,3 +30,71 @@
 //       console.log("Vag Sala") 
 // }
 //  
+
+
+// var friendsName = ["Tonmoy", "Esha", "Sohan", "Shamim", "Karim"];
+// console.log(friendsName, friendsName.length);
+// var indexNumber = friendsName.indexOf("Sohan");
+// console.log(indexNumber);
+// friendsName[2] = "Sohan Gandu";
+// console.log(friendsName)
+// var friendsName = ["Tonmoy", "Esha", "Sohan", "Shamim", "Karim"];
+// friendsName.push("Rohim");
+// friendsName.pop();
+// console.log(friendsName)
+// var num1 = 6;
+// var num2 = 7;
+// console.log(num1 <= num2);
+// var iphone = 79000;
+// myBudget = 50000;
+// if(iphone < myBudget){
+//        console.log("Iphone Ta Tahole NIbo")
+// }
+// else[
+//        console.log("Akta Nokia Phone Kinbo")
+// ]
+// var isGraduated = true
+// var salary = 25000;
+// var cars = 5;
+// if(isGraduated == true && salary > 20000 && cars >= 1){
+//        console.log("Biye Thk Koren")
+// }
+// else{
+//        console.log("Vag Sala")
+// }
+// var money = 1;
+// var danishPrice = 45;
+// var butterBread = 40;
+// var toastBiscit = 30;
+// if(danishPrice < money){
+//        console.log("Ajke Danish Khabo");
+// }
+// else if(butterBread < money){
+//        console.log("Ajke ButterBread Khabo");
+// }
+// else if(toastBiscit < money){
+//        console.log("Toast Biscit Khabo Re")
+// }
+// else{
+//        console.log("Tui Bis Kha Sala")
+// }
+// var math = false;
+// var geometry = false;
+// var straighlint = false;
+// if(math == true){
+//        if(geometry == true){
+//               if(straighlint == true){
+//                      console.log("You Are Pass")
+//               }
+//               else{
+//                      console.log("valo kore poro")
+//               }
+
+//        }
+//        else{
+//               console.log("Tui Ai Onko Ta Paris Na")
+//        }
+// }
+// else{
+//        console.log("Fail")
+// }
